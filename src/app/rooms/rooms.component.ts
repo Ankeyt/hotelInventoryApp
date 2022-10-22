190477f37495fc9ec84a2c8d5ab418ab50ccda4c
Ankeyt <ankitsinha123490@gmail.com>
2022-10-22 23:01:23 +0530

    components description or flow

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelName = 'Marriot wala'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
