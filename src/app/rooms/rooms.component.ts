bc983a82847f1a0d39d965cc02d8c0b0ddfd67a7
Ankeyt <ankitsinha123490@gmail.com>
2022-10-24 15:22:55 +0530

    Built-In directives ngIf-ngFor-ngSwitch-Style

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -12,12 +13,47 @@ export class RoomsComponent implements OnInit {
   numberofRooms=11
 
   hideRooms=false
+
+  room: Room={
+    totalRooms:22,
+    availableRooms:10,
+    bookedRooms:10
+  }
+
+  roomList : RoomList[]=[{
+    roomNumber:1,
+    roomType:'Deluxe Room',
+    amenities:'AC, Wifi,Water,TV,almirah',
+    price:1000,
+    photos:"https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg",
+    checkinTime: new Date('11-Oct-2022'),
+    checkoutTime: new Date('12-Oct-2022')
+  },
+{
+  roomNumber:6,
+  roomType:'Deluxe Room',
+  amenities:'AC, Wifi,Water,TV,almirah',
+  price:100,
+  photos:"https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg",
+  checkinTime: new Date('17-Oct-2022'),
+  checkoutTime: new Date('19-Oct-2022')
+},
+{
+  roomNumber:5,
+  roomType:'Deluxe Room new',
+  amenities:'AC, Wifi,Water,TV,almirah',
+  price:1200,
+  photos:"https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg",
+  checkinTime: new Date('20-Oct-2022'),
+  checkoutTime: new Date('21-Oct-2022')
+}]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   toggle(){
-this.hideRooms=!this.hideRooms
+    this.hideRooms=!this.hideRooms
   }
 }
