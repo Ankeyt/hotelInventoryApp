9027edef74fb07afe1b8e024eb3ceae699c68680
Ankeyt <ankitsinha123490@gmail.com>
2022-10-24 15:50:12 +0530

    pipes uppercase,titlecase,decimal,json

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -27,7 +27,8 @@ export class RoomsComponent implements OnInit {
     price:1000,
     photos:"https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg",
     checkinTime: new Date('11-Oct-2022'),
-    checkoutTime: new Date('12-Oct-2022')
+    checkoutTime: new Date('12-Oct-2022'),
+    rating:4.5
   },
 {
   roomNumber:6,
@@ -36,7 +37,8 @@ export class RoomsComponent implements OnInit {
   price:100,
   photos:"https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg",
   checkinTime: new Date('17-Oct-2022'),
-  checkoutTime: new Date('19-Oct-2022')
+  checkoutTime: new Date('19-Oct-2022'),
+  rating:4.2
 },
 {
   roomNumber:5,
@@ -45,7 +47,8 @@ export class RoomsComponent implements OnInit {
   price:1200,
   photos:"https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg",
   checkinTime: new Date('20-Oct-2022'),
-  checkoutTime: new Date('21-Oct-2022')
+  checkoutTime: new Date('21-Oct-2022'),
+  rating:3.5
 }]
 
   constructor() { }
